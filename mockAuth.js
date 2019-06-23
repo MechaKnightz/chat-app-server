06662fa6c7366492dbc70143e8bec4e156a72765
MechaKnightz <mechaknightz@hotmail.com>
2019-06-24 01:52:40 +0200

    te

--- mockAuth.js
+++ mockAuth.js
@@ -1,27 +1,36 @@
 const Role = require('./_models/role');
-const { secret } = require('./config.json');
+const config = require('config');
 const jwt = require('jsonwebtoken');
-
-const users = [
-    { id: 1, username: 'constan', password: 'constan', firstName: 'Admin', lastName: 'User', role: Role.Admin },
-    { id: 2, username: 'jessie', password: 'jessie', firstName: 'Normal', lastName: 'User', role: Role.User }
-];
+const mysql = require('mysql2');
+const pool = mysql.createPool(config.get("dbPoolConfig"));
+pool.query('SELECT 1', (error, results, fields) => {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
 
 module.exports = {
     authenticate
 };
 
-function getUser(id)
-{
-    return users.find(u => u.id === id);
+function getUser(id) {
+    console.log("id: " + id);
+    pool.execute(
+        'SELECT * FROM `users` WHERE `id` = ?',
+        [id],
+        function (err, results, fields) {
+            return results; // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+        }
+    );
 }
 
 
 function authenticate(token) {
-    console.log(token)
-    var decodedToken = jwt.verify(token, secret);
-    console.log(decodedToken)
+    console.log("token: " + token)
+    var decodedToken = jwt.verify(token, config.get("secret"));
+    console.log("secret: " + config.get("secret"));
+    console.log("decoded token: " + decodedToken);
     var user = getUser(decodedToken.sub);
-    console.log(user)
+    console.log("user: " + user)
     return user;
 }
